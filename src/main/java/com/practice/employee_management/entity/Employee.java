37dca88e3d09f86179f54500c1ba2d01c558a6c6
Your Name <you@example.com>
2026-06-24 21:08:35 +0530

    Implemented create and get employee APIs

--- src/main/java/com/practice/employee_management/entity/Employee.java
+++ src/main/java/com/practice/employee_management/entity/Employee.java
@@ -1,17 +1,33 @@
 package com.practice.employee_management.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String employeeCode;
 
+    @Column(nullable = false)
     private String firstName;
 
     private String lastName;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String department;
@@ -22,5 +38,6 @@ public class Employee {
 
     private BigDecimal salary;
 
+    @Enumerated(EnumType.STRING)
     private EmployeeStatus status;
 }
